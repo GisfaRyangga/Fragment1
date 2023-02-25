8ebe4e985257870479009d17cfceccc799027898
GisfaRyangga <gisfa.putra2603@mail.ugm.ac.id>
2023-02-25 17:03:05 +0700

    add branch dinamis

--- app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
+++ app/src/main/java/com/example/fragmentexample1updated/SimpleFragment.java
@@ -2,8 +2,6 @@ package com.example.fragmentexample1updated;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,48 +10,49 @@ import android.view.ViewGroup;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SimpleFragment} factory method to
+ * create an instance of this fragment.
+ */
 public class SimpleFragment extends Fragment {
+
     private static final int YES = 0;
     private static final int NO = 1;
 
     public SimpleFragment() {
         // Required empty public constructor
     }
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public static SimpleFragment newInstance() {
+        return new SimpleFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false);
-    }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        final RadioGroup radioGroup = view.findViewById(R.id.radio_group);
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                View radioButton = radioGroup.findViewById(i);
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                View radioButton = radioGroup.findViewById(checkedId);
                 int index = radioGroup.indexOfChild(radioButton);
-                TextView textView = view.findViewById(R.id.fragment_header);
-
+                TextView textView = rootView.findViewById(R.id.fragment_header);
                 switch (index) {
                     case YES :
                         textView.setText(R.string.yes_message);
                         break;
-                    case NO :
+                    case NO:
                         textView.setText(R.string.no_message);
+                        break;
                     default:
                         break;
                 }
             }
         });
+
+        return rootView;
     }
 }
